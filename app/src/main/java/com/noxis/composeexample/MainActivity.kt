08ad91be259052c8a036fd1ee258cd59afd7a23d
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-17 22:13:43 +0300

    Card, Box, Image

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -3,61 +3,79 @@ package com.noxis.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                ListItem(name = "Name Actor1", prof = "Actor")
+                ListItem(name = "Name Actor2", prof = "Actor")
+                ListItem(name = "Name Actor3", prof = "Actor")
+                ListItem(name = "Name Actor4", prof = "Actor")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+        ) {
             Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.8f),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                //verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.background(Color.Yellow)
             ) {
-                Column(
-                    modifier = Modifier.background(Color.Cyan).fillMaxWidth(0.5f),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Column Text1")
-                    Text(text = "Column Text2")
-                    Text(text = "Column Text3")
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.batman),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                )
                 Column(
                     modifier = Modifier
+                        .padding(start = 16.dp)
                         .background(Color.Red)
-                        .fillMaxHeight(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(text = "Column1 Text1")
-                    Text(text = "Column1 Text2")
-                    Text(text = "Column1 Text3")
-                }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Column2 Text1")
-                    Text(text = "Column2 Text2")
-                    Text(text = "Column2 Text3")
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
     }
 }
+
