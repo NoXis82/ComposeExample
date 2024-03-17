09815a37a218116d8431f64282a02092e8f13c37
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-17 21:12:17 +0300

    column and row

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -3,44 +3,61 @@ package com.noxis.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.noxis.composeexample.ui.theme.ComposeExampleTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8f),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                //verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.background(Color.Cyan).fillMaxWidth(0.5f),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(text = "Column Text1")
+                    Text(text = "Column Text2")
+                    Text(text = "Column Text3")
+                }
+                Column(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Column1 Text1")
+                    Text(text = "Column1 Text2")
+                    Text(text = "Column1 Text3")
+                }
+                Column(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .fillMaxHeight(0.5f),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Column2 Text1")
+                    Text(text = "Column2 Text2")
+                    Text(text = "Column2 Text3")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeExampleTheme {
-        Greeting("Android")
     }
 }
