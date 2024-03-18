ae4969abb9f333c61c6ff2c512eb5403729ec243
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-18 11:23:22 +0300

    States

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -7,22 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,30 +30,61 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.verticalScroll(rememberScrollState())
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                ListItem(name = "Name Actor1", prof = "Actor")
-                ListItem(name = "Name Actor2", prof = "Actor")
-                ListItem(name = "Name Actor3", prof = "Actor")
-                ListItem(name = "Name Actor4", prof = "Actor")
-                ListItem(name = "Name Actor5", prof = "Actor")
-                ListItem(name = "Name Actor6", prof = "Actor")
-                ListItem(name = "Name Actor7", prof = "Actor")
-                ListItem(name = "Name Actor8", prof = "Actor")
-                ListItem(name = "Name Actor9", prof = "Actor")
-                ListItem(name = "Name Actor10", prof = "Actor")
-                ListItem(name = "Name Actor11", prof = "Actor")
-                ListItem(name = "Name Actor12", prof = "Actor")
+                CircleItem()
             }
         }
     }
+}
+
+@Composable
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color.value, shape = CircleShape)
+            .clickable {
+                when(++counter.value) {
+                    StateColor.STATE_1.value -> color.value = StateColor.STATE_1.color
+                    StateColor.STATE_2.value -> color.value = StateColor.STATE_2.color
+                    StateColor.STATE_3.value -> color.value = StateColor.STATE_3.color
+                }
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 25.sp
+            )
+        )
+    }
+}
+
+enum class StateColor(val value: Int, val color: Color) {
+    STATE_1(10, Color.Black),
+    STATE_2(20, Color.Red),
+    STATE_3(30, Color.Cyan)
+
+
 }
 
 @Composable
