889a34109fa3e70b899d8b4c1782d923026df7e1
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-18 15:29:53 +0300

    weather app jetpack compose

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -1,152 +1,94 @@
 package com.noxis.composeexample
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.noxis.composeexample.ui.MyModel
-import com.noxis.composeexample.ui.MyRow
-import com.noxis.composeexample.ui.theme.Gray100
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val itemsRow = listOf(
-            MyModel(imageId = R.drawable.image_1, title = "Димон", content = "Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности играет важную роль в формировании ключевых компонентов планируемого обновления!"),
-            MyModel(imageId = R.drawable.image_2, title = "Беня", content = "Test"),
-            MyModel(imageId = R.drawable.image_3, title = "Кузя", content = "Test"),
-            MyModel(imageId = R.drawable.image_4, title = "Иеся", content = "Test"),
-            MyModel(imageId = R.drawable.image_5, title = "Алик", content = "Test"),
-            MyModel(imageId = R.drawable.image_6, title = "Гера", content = "Test")
-        )
         setContent {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize().background(Gray100).padding(4.dp)
-            ) {
-                itemsIndexed(itemsRow) { _, item ->
-                    MyRow(item = item)
-                }
-            }
+            Greeting(name = "Madrid", this)
         }
     }
-}
 
-@Composable
-private fun CircleItem() {
-    val counter = remember {
-        mutableStateOf(0)
-    }
-    val color = remember {
-        mutableStateOf(Color.Blue)
-    }
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .background(color.value, shape = CircleShape)
-            .clickable {
-                when (++counter.value) {
-                    StateColor.STATE_1.value -> color.value = StateColor.STATE_1.color
-                    StateColor.STATE_2.value -> color.value = StateColor.STATE_2.color
-                    StateColor.STATE_3.value -> color.value = StateColor.STATE_3.color
-                }
-            },
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = counter.value.toString(),
-            style = TextStyle(
-                color = Color.White,
-                fontSize = 25.sp
-            )
-        )
+    companion object {
+        const val API_KEY = "2d25b81b309c4ac5aee122007241803"
     }
 }
 
-enum class StateColor(val value: Int, val color: Color) {
-    STATE_1(10, Color.Black),
-    STATE_2(20, Color.Red),
-    STATE_3(30, Color.Cyan)
-
+private fun getResult(city: String, state: MutableState<String>, context: Context) {
+    val url = "https://api.weatherapi.com/v1/current.json" +
+            "?key=${MainActivity.API_KEY}&" +
+            "q=$city" +
+            "&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        com.android.volley.Request.Method.GET,
+        url,
+        { response ->
+            val obj = JSONObject(response)
+            state.value = obj.getJSONObject("current").getString("temp_c")
+        },
+        { error ->
+            state.value = error.networkResponse.statusCode.toString()
+        }
+    )
+    queue.add(stringRequest)
 
 }
 
 @Composable
-private fun ListItem(name: String, prof: String) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-//            .clickable {
-//                Log.d("TAG", "ListItem: $name, $prof")
-//            }
-            .pointerInput(Unit) {
-                detectTapGestures {
-                    Log.d("TAG", "ListItem Tap: $it")
-                }
-//                detectDragGesturesAfterLongPress { change, dragAmount ->
-//                    Log.d("TAG", "ListItem Long press: $dragAmount")
-//                }
-            },
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(5.dp)
-    ) {
+fun Greeting(name: String, context: Context) {
+    val state = remember {
+        mutableStateOf("Unknown")
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.batman),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(64.dp)
-                        .padding(4.dp)
-                        .clip(CircleShape)
-                )
-                Column(
-                    modifier = Modifier
-                        .padding(start = 16.dp)
-                ) {
-                    Text(text = name)
-                    Text(text = prof)
+            Text(text = "Temp in $name = ${state.value} C")
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(
+                modifier = Modifier
+                    .padding(6.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    getResult(name, state, context)
                 }
+            ) {
+                Text(text = "Refresh")
             }
         }
     }
 }
-
