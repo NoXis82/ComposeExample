d265c5a12d885ec0de2ca3433832ec5e07001cea
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-18 19:07:17 +0300

    Добавляем Text для показа даты и времени

--- app/src/main/java/com/noxis/composeexample/screens/MainScreen.kt
+++ app/src/main/java/com/noxis/composeexample/screens/MainScreen.kt
@@ -1,20 +1,27 @@
 package com.noxis.composeexample.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.noxis.composeexample.R
 import com.noxis.composeexample.ui.theme.BlueLight
 
@@ -41,7 +48,22 @@ fun MainScreen() {
             elevation = CardDefaults.cardElevation(0.dp),
             shape = RoundedCornerShape(10.dp)
         ) {
-
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 6.dp, start = 6.dp),
+                        text = "20.06.2024 13:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                }
+            }
         }
     }
 }
