4716179965ad16db742fe783609d292c0f9c1a14
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-18 18:38:53 +0300

    первичная карточка Card

--- app/src/main/java/com/noxis/composeexample/screens/MainScreen.kt
+++ app/src/main/java/com/noxis/composeexample/screens/MainScreen.kt
@@ -1,14 +1,22 @@
 package com.noxis.composeexample.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.noxis.composeexample.R
+import com.noxis.composeexample.ui.theme.BlueLight
 
 
 @Composable
@@ -22,4 +30,18 @@ fun MainScreen() {
             .alpha(0.8f),
         contentScale = ContentScale.Crop
     )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(6.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(BlueLight),
+            elevation = CardDefaults.cardElevation(0.dp),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+
+        }
+    }
 }
