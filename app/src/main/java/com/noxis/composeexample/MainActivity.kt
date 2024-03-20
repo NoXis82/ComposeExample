eafcfd18561488284d129f7e8d7e75403bc37a33
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-20 15:48:28 +0300

    SnackBar

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Menu
@@ -13,11 +16,22 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +46,19 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(applicationContext: Context) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) {data ->
+                Snackbar(
+                    snackbarData = data,
+                    containerColor = Color.Gray,
+                    shape = RoundedCornerShape(10.dp)
+                )
+            }
+        },
         topBar = {
             TopAppBar(
                 title = { Text(text = "AppBar") },
@@ -46,12 +72,36 @@ fun MainScreen(applicationContext: Context) {
                 },
                 actions = {
                     IconButton(onClick = {
-                        Toast.makeText(applicationContext, "Search", Toast.LENGTH_SHORT).show()
+                        coroutineScope.launch {
+                            snackbarHostState.showSnackbar(
+                                "Search",
+                                duration = SnackbarDuration.Short
+                            )
+                        }
                     }) {
                         Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
                     }
                     IconButton(onClick = {
-                        Toast.makeText(applicationContext, "Build", Toast.LENGTH_SHORT).show()
+                        coroutineScope.launch {
+                            val result =
+                                snackbarHostState.showSnackbar(
+                                    "Search",
+                                    actionLabel = "Action",
+                                    duration = SnackbarDuration.Short
+                                )
+                            when (result) {
+                                SnackbarResult.ActionPerformed -> {
+                                    Toast.makeText(applicationContext, "Build ActionPerformed", Toast.LENGTH_SHORT)
+                                        .show()
+
+                                }
+
+                                SnackbarResult.Dismissed -> {
+                                    Toast.makeText(applicationContext, "Build Dismissed", Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+                            }
+                        }
                     }) {
                         Icon(imageVector = Icons.Filled.Build, contentDescription = "Build")
                     }
