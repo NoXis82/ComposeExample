5c6951c92eb59113fa10aefcf477f6e3df81c8cf
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-20 15:20:48 +0300

    top app bar

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -1,46 +1,64 @@
 package com.noxis.composeexample
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.noxis.composeexample.ui.theme.ComposeExampleTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen(applicationContext)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    ComposeExampleTheme {
-        Greeting("Android")
+fun MainScreen(applicationContext: Context) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "AppBar") },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Gray),
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(applicationContext, "Menu", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        Toast.makeText(applicationContext, "Search", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+                    }
+                    IconButton(onClick = {
+                        Toast.makeText(applicationContext, "Build", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.Build, contentDescription = "Build")
+                    }
+                }
+            )
+        }
+    ) {
+        it
     }
 }
