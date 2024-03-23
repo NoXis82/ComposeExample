66bfd853fe797a6acaa73b97cdc742bae62facdd
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-23 21:29:14 +0300

    ConstrainLayout

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -3,44 +3,56 @@ package com.noxis.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.noxis.composeexample.ui.theme.ComposeExampleTheme
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingPreview()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ComposeExampleTheme {
-        Greeting("Android")
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (text, button, image) = createRefs()
+        val bottomGuideLine = createGuidelineFromBottom(0.6f)
+        Button(onClick = { }, modifier = Modifier.constrainAs(button) {
+            bottom.linkTo(bottomGuideLine)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            top.linkTo(bottomGuideLine)
+        }) {
+            Text(text = "Click")
+        }
+        Text(text = "Hello world", modifier = Modifier.constrainAs(text) {
+            bottom.linkTo(button.top)
+            start.linkTo(button.start)
+            end.linkTo(button.end)
+        })
+        Image(
+            painter = painterResource(id = R.drawable.simpson),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 6.dp, end = 6.dp)
+                .constrainAs(image) {
+                    bottom.linkTo(text.top)
+                    start.linkTo(button.start)
+                    end.linkTo(button.end)
+                }
+        )
     }
 }
