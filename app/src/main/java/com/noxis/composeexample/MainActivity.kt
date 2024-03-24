e56b82aefc2593040f6981aa34e893939f029677
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-24 21:44:46 +0300

    Сканер QR кодов - zxing

--- app/src/main/java/com/noxis/composeexample/MainActivity.kt
+++ app/src/main/java/com/noxis/composeexample/MainActivity.kt
@@ -1,46 +1,58 @@
 package com.noxis.composeexample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 import com.noxis.composeexample.ui.theme.ComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val barcodeLauncher = registerForActivityResult(ScanContract()) { result ->
+        result.contents?.let {
+            Toast.makeText(this, "Scanned:$it", Toast.LENGTH_SHORT).show()
+        } ?: run {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(contentAlignment = Alignment.Center) {
+                    Button(onClick = {
+                        scan()
+                    }) {
+                        Text(text = "Scan")
+                    }
                 }
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeExampleTheme {
-        Greeting("Android")
+    private fun scan() {
+        val options = ScanOptions()
+        options.apply {
+            setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+            setPrompt("Scan a barcode")
+            setCameraId(0)  // Use a specific camera of the device
+            setBeepEnabled(false)
+            setBarcodeImageEnabled(true)
+        }
+        barcodeLauncher.launch(options)
     }
 }
